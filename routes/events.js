a6dccafd7b507670d1f522278d44225ca68cafae
Ethan Sternke <ethan.sternke@gmail.com>
2018-07-03 14:05:36 -0500

    access control

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// bring in event model
+// bring in models
 let Event = require('../models/event');
+let User = require('../models/user');
 
 // add route
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('add_event', {
         title: 'add event'
     });
@@ -15,7 +16,7 @@ router.get('/add', (req, res) => {
 router.post('/add', (req, res) => {
 
     req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('author', 'Author is required').notEmpty();
+    // req.checkBody('author', 'Author is required').notEmpty();
     req.checkBody('lat', 'A latitude value is required').notEmpty();
     req.checkBody('lng', 'A longitude value is required').notEmpty();
     req.checkBody('game', 'Game is required').notEmpty();
@@ -32,7 +33,7 @@ router.post('/add', (req, res) => {
     } else {
         let event = new Event();
         event.title = req.body.title;
-        event.author = req.body.author;
+        event.author = req.user._id;
         event.lat = req.body.lat;
         event.lng = req.body.lng;
         event.game = req.body.game;
@@ -54,12 +55,17 @@ router.post('/add', (req, res) => {
 });
 
 // load edit form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Event.findById(req.params.id, (err, event) => {
-        res.render('edit_event', {
-            title: 'Edit Event',
-            event: event
-        });
+        if (event.author != req.user._id) {
+            req.flash('danger', 'Not Authorized');
+            return res.redirect('/');
+        } else {
+            res.render('edit_event', {
+                title: 'Edit Event',
+                event: event
+            });
+        }
     });
 });
 
@@ -67,7 +73,7 @@ router.get('/edit/:id', (req, res) => {
 router.post('/edit/:id', (req, res) => {
     let event = {};
     event.title = req.body.title;
-    event.author = req.body.author;
+    // event.author = req.body.author;
     event.lat = req.body.lat;
     event.lng = req.body.lng;
     event.game = req.body.game;
@@ -82,30 +88,55 @@ router.post('/edit/:id', (req, res) => {
             console.log(err);
             return;
         } else {
-            req.flash('success', 'Article Updated');
+            req.flash('success', 'Event Updated');
             res.redirect('/');
         }
     });
 });
 
-router.delete('/:id', (req, res) => {
+// delete event
+router.delete('/:id', function (req, res) {
+    if (!req.user._id) {
+        res.status(500).send();
+    }
+
     let query = { _id: req.params.id }
 
-    Event.remove(query, (err) => {
-        if (err) {
-            console.log(err);
+    Event.findById(req.params.id, function (err, event) {
+        if (event.author != req.user._id) {
+            res.status(500).send();
+        } else {
+            Event.remove(query, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.send('Success');
+            });
         }
-        res.send('Success');
     });
+
 });
 
 // get single event
 router.get('/:id', (req, res) => {
     Event.findById(req.params.id, (err, event) => {
-        res.render('event', {
-            event: event
+        User.findById(event.author, function (err, user) {
+            res.render('event', {
+                event: event,
+                author: user.name
+            });
         });
     });
 });
+
+// access control
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        req.flash('danger', 'Please log in');
+        res.redirect('/users/login');
+    }
+}
 
 module.exports = router;
